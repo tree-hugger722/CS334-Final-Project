5db5fac168453e63d154fcc563948560ba487075
Anna Owens <aco3@williams.edu>
2022-11-20 14:55:08 -0500

    update expression start

--- Parser.fs
+++ Parser.fs
@@ -1,1 +1,22 @@
 module Parser
+open Combinator
+
+type Season = 
+|Fall | Winter | Spring | Summer
+
+type Green = 
+|Kale of "Kale" * Fall
+|Arugula of "Arugula" * Spring
+|Spinach of "Spinach" * Winter
+
+
+
+
+type Salad = green * vegetable * dressing
+
+type Dish = 
+|salad of Salad
+
+type Expr = 
+|season of Season
+|
