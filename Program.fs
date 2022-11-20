4a190e5c63e2d889a007fab7f9fd61dfe1441503
Emma M. Neil <emmaneil722@gmail.com>
2022-11-20 14:32:04 -0500

    first commit, skeleton

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿open Combinator
+open Parser
+open Evaluator
+
+[<EntryPoint>]
+
+let main args = 
+    if args.Length < 1 then
+        printfn "Usage: dotnet run <season>"
+        printfn "Season options: fall, winter, spring, summer"
+        exit 1
+    let expression = parse args.[0]
+    match expression with
+    | Some ast -> printf "%A\n" (prettyprint (eval ast))
+    | None -> printfn "Invalid program"
+    0
