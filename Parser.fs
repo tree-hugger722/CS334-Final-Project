e5c047ae5ef7f5718ed778f55486092cbe69f779
Anna Owens <aco3@williams.edu>
2022-12-10 13:40:01 -0500

    Parser progress

--- Parser.fs
+++ Parser.fs
@@ -17,13 +17,14 @@ type Name =
 |Category of Category
 |Name of string
 |Combo of Name * Name
+|NoName
 
 type Flag = 
 |Include 
 |Exclude
 
 type Exception = 
-|SoftCore of Flag * Name
+|SoftCore of Flag * List<Name>
 |HardCore of Exception * Exception
 
 type DishType = 
@@ -43,7 +44,9 @@ type Temperature =
 |Cold
 
 type Attribute = 
-|Temp of Temperature 
+|Attribute of Temperature 
+|Attributes of Attribute * Attribute
+|NoAttribute 
 
 type Expr = 
 |Recipe of Attribute * Dish 
@@ -67,8 +70,8 @@ let dish a =
 
 let temp a = 
     match a with 
-    |"warm" -> Temp(Warm)
-    |"cold" -> Temp(Cold)
+    |"warm" -> Warm
+    |"cold" -> Cold
     |_ -> failwith "Not a valid temperature"
 
 let flag a = 
@@ -92,20 +95,27 @@ let name a =
     |str -> Name(str)
 
 //converts a list of softcore exceptions into hardcore exception
+let rec convertAttributes xs = 
+    match xs with 
+    |[x] -> Attribute(temp x)
+    |x::xs' -> Attributes(Attribute(temp x), convertAttributes xs')
+    |_ -> NoAttribute
+
+//converts list of attributes to readable
 let rec convertHardcore xs = 
     match xs with 
     |[x] -> x
     |x::xs' -> HardCore(x, convertHardcore xs')
-    |_ -> SoftCore(Include, Name("confused"))
+    |_ -> SoftCore(Include, [Name("confused")])
 
 
-let expr, exprImpl = recparser()
+//let expr, exprImpl = recparser()
 
-let grammar = pleft expr peof
+//let grammar = pleft expr peof
 
-let ptemp = pstr "warm" <|> pstr "cold"
+let ptemp = pstr "warm " <|> pstr "cold "
 
-let pattribute = ptemp 
+let pattribute = (pmany0 ptemp)   
 
 let pseas = pstr "fall" <|> pstr "winter"  <|> pstr "summer" <|> pstr "spring" 
 
@@ -119,22 +129,23 @@ let pname = pmany1 pletter |>> (fun xs -> System.String.Join("", xs))
 
 //parse flag, ignore space, parse name and return softfore(flag, name)
 //this doesn't include with cheese, nuts or with a and without b
-let psoftcore = pseq (pleft (pflag) (pws1)) (pname) (fun (a,b) -> SoftCore(flag a, name b))
+let psoftcore = pseq (pleft (pflag) (pws1)) (pname) (fun (a,b) -> SoftCore(flag a, [name b]))
 
 
-let pdish = pseq (pseq (pseason) (pdishType) (fun (a, b) -> (a, b))) (psoftcore) (fun ((a, b), c) -> Dish(season a, dish b, c))
+//let pdish = pseq (pseq (pseason) (pdishType) (fun (a, b) -> (a, b))) (psoftcore) (fun ((a, b), c) -> Dish(season a, dish b, c))
 
-exprImpl:= pseq ((pleft (pattribute) (pws1))) (pdish) (fun (a,b) -> Recipe(temp a, b))
+//exprImpl:= pseq (pattribute) (pdish) (fun (a,b) -> Recipe(temp a, b))
 
+//exprImpl:= Recipe(Attribute(Warm), pdish)
 
 (*
  Function parses input and returns success or failure
  *)
 
- 
+ (*
 let parse(s: string) : Expr option = 
     let input = prepare s
     match grammar input with
     | Success(res, _) -> Some res
     | Failure(_, _) -> None
-
+*)
