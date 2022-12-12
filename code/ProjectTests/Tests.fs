ebcd4091d70a4ca1cc724cf1a12a8577c3d659a5
Anna Owens <aco3@williams.edu>
2022-12-11 23:28:15 -0500

    New Full Test

--- code/ProjectTests/Tests.fs
+++ code/ProjectTests/Tests.fs
@@ -23,6 +23,24 @@ type TestClass () =
             Assert.AreEqual(expected, ast)
         |None ->
             Assert.IsTrue(false)
+    
+    [<TestMethod>]
+    member this.testWarmSpringSaladWithNuts () =
+        let input = "warm spring salad with nuts"
+        let output = prettyprint (evaluate input)
+
+//put each line of string output as an element in the array
+        let output_substring = output.Split 'n'
+//get the array length (# of lines)
+        let length = output_substring.Length
+
+//recipe should have six lines, fail test otherwise
+        if length = 6 then
+            Assert.IsTrue(true)
+        else   
+            Assert.IsTrue(false)
+
+
 
 
     
